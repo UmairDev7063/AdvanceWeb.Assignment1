842f0d0b5b538ef4d5b4c85db00364eaaded3216
UmairDev7063 <umairiftikhar7063@gmail.com>
2026-04-15 04:26:35 +0500

    set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         <div className="container">
           <div className="row row1">
             <div className="img-box">
-              <img src="/images/showcase-photo3.jpg" alt="Image Description" />
+              <img src="/images/showcase-photo3.jpg" alt="Travel destination in Deganvy, U.K." />
             </div>
             <div className="text-box">
               <h2 className="lg-heading text-black">DEGANVY, U.K</h2>
@@ -30,7 +30,7 @@ const Home = () => {
           </div>
           <div className="row row2">
             <div className="img-box">
-              <img src="/images/showcase-photo1.jpg" alt="Image Description" />
+              <img src="/images/showcase-photo1.jpg" alt="Travel destination in the Desert, Egypt." />
             </div>
             <div className="text-box">
               <h2 className="lg-heading text-black">DESERT, EGYPT</h2>
@@ -62,7 +62,7 @@ const Home = () => {
             </div>
             <div className="box box-3">
               <i className="fas fa-user-check fa-2x text-red"></i>
-              <h2 className="md-heading">Experice</h2>
+              <h2 className="md-heading">Experience</h2>
               <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, quia. Corporis dolores ipsa a veniam molestias beatae, fuga ex laboriosam.
               </p>
